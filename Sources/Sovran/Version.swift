f9976ffe377aedb2d38d5135961fc029804d89c1
Brandon Sneed <brandon.sneed@segment.com>
2021-08-16 11:16:09 -0700

    Updated release information

--- Sources/Sovran/Version.swift
+++ Sources/Sovran/Version.swift
@@ -1,2 +1,17 @@
-// Version information for the Sovran-Swift library.
+//
+//  Version.swift
+//  Sovran
+//
+//  Created by Brandon Sneed on 08/16/21.
+//
+
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+
+// Use release.sh's automation.
+
+// BREAKING.FEATURE.FIX
+
 internal let __sovran_version = "1.0.1"
