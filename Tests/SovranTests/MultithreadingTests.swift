41b8676ea9aaac0d8ca99490fd825e90811910e9
Brandon Sneed <brandon@redf.net>
2024-08-11 16:23:54 -0700

    Add subscriber interactions to multi threading test

--- Tests/SovranTests/MultithreadingTests.swift
+++ Tests/SovranTests/MultithreadingTests.swift
@@ -62,6 +62,29 @@ final class MultithreadingTests: XCTestCase {
 
         let numThreads = 10
         let numActionsPerThread = 10000
+        
+        class Dummy: Subscriber { }
+        let dummy = Dummy()
+        var sub1_inc = 0
+        let sub1lock = NSLock()
+        store.subscribe(dummy) { (state: MyState) in
+            sub1lock.lock()
+            defer { sub1lock.unlock() }
+            sub1_inc += 1
+            //print(sub1_inc)
+        }
+        
+        var sub2_inc = 0
+        let sub2lock = NSLock()
+        store.subscribe(dummy) { (state: MyState) in
+            sub2lock.lock()
+            defer { sub2lock.unlock() }
+            sub2_inc += 1
+        }
+        
+        
+        let expectedMyStateValue = numThreads * numActionsPerThread
+        let expectedAnotherStateCount = numThreads * numActionsPerThread
 
         let startTime = DispatchTime.now()
         
@@ -77,6 +100,10 @@ final class MultithreadingTests: XCTestCase {
         // Wait for all threads to finish
         group.wait()
         
+        while (sub1_inc != expectedMyStateValue) || (sub2_inc != expectedAnotherStateCount) {
+            RunLoop.current.run(until: .distantPast)
+        }
+        
         let endTime = DispatchTime.now()
         let elapsedTime = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
         let elapsedTimeInMilliSeconds = Double(elapsedTime) / 1_000_000.0
@@ -86,14 +113,17 @@ final class MultithreadingTests: XCTestCase {
         let finalMyState: MyState = store.currentState()!
         let finalAnotherState: AnotherState = store.currentState()!
 
-        let expectedMyStateValue = numThreads * numActionsPerThread
-        let expectedAnotherStateCount = numThreads * numActionsPerThread
-
-        assert(finalMyState.value == expectedMyStateValue, "MyState value: \(finalMyState.value), Expected: \(expectedMyStateValue)")
-        assert(finalAnotherState.count == expectedAnotherStateCount, "AnotherState count: \(finalAnotherState.count), Expected: \(expectedAnotherStateCount)")
+        XCTAssert(finalMyState.value == expectedMyStateValue, "MyState value: \(finalMyState.value), Expected: \(expectedMyStateValue)")
+        XCTAssert(finalAnotherState.count == expectedAnotherStateCount, "AnotherState count: \(finalAnotherState.count), Expected: \(expectedAnotherStateCount)")
 
         print("Tests passed!")
         print("Final MyState value: \(finalMyState.value)")
         print("Final AnotherState count: \(finalAnotherState.count)")
+        
+        print("Subscriber 1 called \(sub1_inc) times")
+        print("Subscriber 2 called \(sub2_inc) times")
+        
+        XCTAssertEqual(sub1_inc, expectedMyStateValue)
+        XCTAssertEqual(sub2_inc, expectedAnotherStateCount)
     }
 }
